803b56499f4d5b1c5cc8160beb7da4579e42ba98
Leonardo Corti <leonardo.filippo@ymail.com>
2024-11-16 16:53:34 +0100

    added icon to executable

--- build.rs
+++ build.rs
@@ -1,3 +1,6 @@
 fn main() {
-    slint_build::compile("ui/main.slint").expect("Slint build failed");
+    slint_build::compile("ui/main.slint")
+        .expect("Slint build failed");
+    embed_resource::compile("icon.rc", embed_resource::NONE)
+        .manifest_optional().unwrap();
 }
